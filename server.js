d9a28ba46decbb94d9542f07ee2a70115ff25eac
Dongle Su <agdsdl@sina.com.cn>
2016-06-17 16:17:55 +0800

    add ip display for server

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 var net = require('net');
 var process = require('process');
 
-net.createServer(function(conn){
+const server = net.createServer(function(conn){
   console.log('client connected:' + conn.remoteAddress + ':' + conn.remotePort);
 
   conn.on('data', function(data){
@@ -16,4 +16,27 @@ net.createServer(function(conn){
   process.stdin.pipe(conn);
 }).listen(9000);
 
-console.log("server listenning at 9000...");
+console.log("server listenning at port 9000");
+
+var os = require('os');
+var ifaces = os.networkInterfaces();
+
+Object.keys(ifaces).forEach(function (ifname) {
+  var alias = 0;
+
+  ifaces[ifname].forEach(function (iface) {
+    if ('IPv4' !== iface.family || iface.internal !== false) {
+      // skip over internal (i.e. 127.0.0.1) and non-ipv4 addresses
+      return;
+    }
+
+    if (alias >= 1) {
+      // this single interface has multiple ipv4 addresses
+      console.log(ifname + ':' + alias, iface.address);
+    } else {
+      // this interface has only one ipv4 adress
+      console.log(ifname, iface.address);
+    }
+    ++alias;
+  });
+});
