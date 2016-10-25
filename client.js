d4af955a4e1299d547d520a48759fdfe3b3f799b
Dongle Su <agdsdl@sina.com.cn>
2016-10-25 19:48:49 +0800

    添加文件传输功能，使用FILE:filepath来传文件

--- client.js
+++ client.js
@@ -1,4 +1,8 @@
 var net = require('net');
+const fs = require('fs');
+const path = require('path');
+
+const savePath = "/Users/dongle/Downloads/nodejs/";
 
 var options = {
   port: 9000,
@@ -7,8 +11,47 @@ var options = {
 
 var client = net.connect(options, function(){
   client.write('');
+  
+  var state = 0;// 0 normal, 1 recive file
+  var filename = "";
+  var receiveBuf = new Buffer(0);
+  client.on('data', function(data){
+    if (state === 0){
+      var str = data.toString();
+      console.log(str);
+      if(str.startsWith("FILE:")){
+        str = str.substr(5).trim();
+        var p = path.parse(str);
+        //fs.mkdirSync(savePath);
+        filename = savePath + p.base;
+        state = 1;
+      }
+    }
+    else if (state == 1){
+      receiveBuf = Buffer.concat([receiveBuf, data]);
+      // const lastbytes = receiveBuf.subarray(receiveBuf.length-5, receiveBuf.length-1);
+      // const lastStr = String.fromCharCode.apply(null, lastbytes);
+      var index = receiveBuf.indexOf("#END#");
+      if(index >= 0){
+      //if(lastbytes.length == 6 && lastStr == "#END#\n"){
+      //if(receiveBuf.includes("#END#", receiveBuf.length-6)){
+        const fd = fs.openSync(filename, 'w');
+        //fs.write(fd, receiveBuf, 0, receiveBuf.length-5);
+        var written = fs.writeSync(fd, receiveBuf, 0, index);
+        console.log("bytes writed "+written);
+        receiveBuf = new Buffer(0);
+        state = 0;
+        fs.close(fd, function(error){
+          if (error){
+            console.log(error);
+          }
+        });
+      }
+    }
+  });
+
   process.stdin.pipe(client);
-  client.pipe(process.stdout);
+  //client.pipe(process.stdout);
 });
 
 client.on('error', function(err){
@@ -24,4 +67,23 @@ client.on('close', function(){
   client.unpipe();
   client.destroy();
   process.stdin.destroy();
-})
+});
+
+process.stdin.on('data', function(data){
+  var str = data.toString();
+
+  if(str.startsWith("FILE:")){
+    str = str.substr(5).trim();
+    fs.readFile(str, function(error, data){
+      if (error){
+        console.log(error.message);
+      }
+      else if(data){
+        client.setNoDelay(true);
+        client.write(data, function(){
+          client.write("#END#");
+        });
+      }
+    });
+  }
+});
--- server.js
+++ server.js
@@ -1,20 +1,78 @@
 var net = require('net');
 var process = require('process');
+const fs = require('fs');
+const path = require('path');
 
+const savePath = "/Users/dongle/Downloads/nodejs/";
+
+var curConn;
 const server = net.createServer(function(conn){
+  var state = 0;// 0 normal, 1 recive file
+  var filename = "";
+  var receiveBuf = new Buffer(0);
+
+  curConn = conn;
   console.log('client connected:' + conn.remoteAddress + ':' + conn.remotePort);
 
   conn.on('data', function(data){
-    console.log(data.toString());
-  })
+    if (state === 0){
+      var str = data.toString();
+      console.log(str);
+      if(str.startsWith("FILE:")){
+        str = str.substr(5).trim();
+        var p = path.parse(str);
+        //fs.mkdirSync(savePath);
+        filename = savePath + p.base;
+        state = 1;
+      }
+    }
+    else if (state == 1){
+      receiveBuf = Buffer.concat([receiveBuf, data]);
+      var index = receiveBuf.indexOf("#END#");
+      if(index >= 0){
+      //if(receiveBuf.includes("#END#", receiveBuf.length-6)){
+        const fd = fs.openSync(filename, 'w');
+        //fs.write(fd, receiveBuf, 0, receiveBuf.length-5);
+        var written = fs.writeSync(fd, receiveBuf, 0, index);
+        console.log("bytes writed "+written);
+        receiveBuf = new Buffer(0);
+        state = 0;
+        fs.close(fd, function(error){
+          if (error){
+            console.log(error);
+          }
+        });
+      }
+    }
+  });
 
   conn.on('close', function(data){
     console.log('client leaved:' + conn.remoteAddress);
     process.stdin.unpipe(conn);
-  })
+  });
 
   process.stdin.pipe(conn);
+
 }).listen(9000);
+
+process.stdin.on('data', function(data){
+  var str = data.toString();
+
+  if(str.startsWith("FILE:")){
+    str = str.substr(5).trim();
+    fs.readFile(str, function(error, data){
+      if (error){
+        console.log(error.message);
+      }
+      else if(data){
+        // curConn.setNoDelay(true);
+        curConn.write(data, function(){
+          curConn.write("#END#");
+        });
+      }
+    });
+  }
+});
 
 console.log("server listenning at port 9000");
 
